8896b0a35b2aee84d1a1d2ae9d6c9487aa548bab
TakuHiwa <takuyah106@tutanota.com>
2024-09-12 11:06:45 +0800

    INT BIST Start / INT BIST Success! / (not fail) Quartus Run

--- sub_module_bist.vhd
+++ sub_module_bist.vhd
@@ -5,33 +5,43 @@ use ieee.numeric_std.all;
 entity sub_module_bist is
     port (
         clk, reset_n : in std_logic;
-        en_bist_n, start_bist_n : in std_logic;
-        success : out std_logic
+        -- SYSTEM
+        en_bist : in std_logic;
+        test_start : in std_logic;
+        fail : out std_logic;
+        -- SRAM
+        data_read : in std_logic_vector(7 downto 0);
+        addr: out std_logic_vector(7 downto 0);
+        data_write : out std_logic_vector(7 downto 0);
+        rw : out std_logic
     );
 end;
 
-architecture synth of sub_module_bist is  
+architecture synth of sub_module_bist is   
+    component marchcontroller is
+        port (clk, reset_n: in std_logic;
+            addr: out std_logic_vector(7 downto 0);
+            data_write: out std_logic_vector(7 downto 0);
+            rw : out std_logic;
+            ready : out std_logic
+        ); 
+    end component;
 
+    component output_analyzer is
+        port (
+            clk        : in std_logic;
+            reset_n    : in std_logic;
+            ready      : in std_logic;
+            rw         : in std_logic;
+            data_read  : in std_logic_vector(7 downto 0);
+            fail       : out std_logic
+        );
+    end component;
+
+    signal ready_tmp : std_logic;
+    signal rw_tmp : std_logic;
 begin
-        process (clk, reset_n, en_bist_n ) is
-            variable count : integer := 0;
-        begin
-            if (reset_n = '0') then
-                success <= '0';
-                count := 0;
-            elsif rising_edge(clk) then
-					 if (en_bist_n = '0' and start_bist_n = '0') then
-						 if (count < 1000000 * 100) then
-							count := count + 1;
-							success <= '0';
-						 else
-							success <= '1';
-							count := 0;
-						 end if;
-					 else
-						success <= '0';
-						count := 0;
-					 end if;
-				end if;
-		 end process;
+    rw <= rw_tmp;
+    mc1: marchcontroller port map(clk, reset_n and (not en_bist) and (not test_start), addr, data_write, rw_tmp, ready_tmp);
+    oa1: output_analyzer port map(clk, reset_n and (not en_bist) and (not test_start), ready_tmp, rw_tmp, data_read, fail);
 end;
--- sub_module_ram.vhd
+++ sub_module_ram.vhd
@@ -1,22 +1,20 @@
+-- vhdl-linter-disable type-resolved
 library IEEE; use IEEE.STD_LOGIC_1164.all;
 
 entity sub_module_ram is
-    port (clk, reset: in STD_LOGIC;
+    port (clk, reset_n: in STD_LOGIC;
 	          -- SYSTEM
           addr, data_write: in STD_LOGIC_VECTOR(7 downto 0);
           data_read_r, data_read: out STD_LOGIC_VECTOR(7 downto 0);
           mem, rw: in STD_LOGIC;
           ready: out STD_LOGIC;
-			 ext : in STD_LOGIC;
+          ext : in STD_LOGIC;
           
           -- SRAM
           ad: out STD_LOGIC_VECTOR(19 downto 0);
           dio: inout STD_LOGIC_VECTOR(15 downto 0);
           we_n, oe_n, ce_n, lb_n, ub_n: out STD_LOGIC;
-
-          -- SEGMENT
-          segoutL, segoutM: out STD_LOGIC_VECTOR(7*2-1 downto 0);
-            segoutR: out STD_LOGIC_VECTOR(7*4-1 downto 0)
+          data_display : out STD_LOGIC_VECTOR(7 downto 0)
     );
 end;
 
@@ -34,7 +32,7 @@ architecture synth of sub_module_ram is
     end component;
 	
 	component memorycontroller is
-        port (clk, reset: in STD_LOGIC;
+        port (clk, reset_n: in STD_LOGIC;
             -- SYSTEM
             addr, data_write: in STD_LOGIC_VECTOR(7 downto 0);
             data_read_r, data_read: out STD_LOGIC_VECTOR(7 downto 0);
@@ -48,37 +46,16 @@ architecture synth of sub_module_ram is
         );
 	end component;
  
-    component seg7 is 
-        port (I: in STD_LOGIC_VECTOR(3 downto 0);
-            Seg: out STD_LOGIC_VECTOR(6 downto 0));
-    end component;
-	
-	signal data_display : std_logic_vector(7 downto 0);
-    signal lb_n_r, ub_n_r, ce_n_r, oe_n_r, we_n_r : std_logic;
-	signal dio_r : std_logic_vector(15 downto 0);
-	signal ad_r : std_logic_vector(19 downto 0);
-	
 	signal mem_ext : std_logic;
-	signal data_read_EA : std_logic_vector(7 downto 0) := "11101010";
 	signal data_read_itnram : std_logic_vector(7 downto 0)  := (others => '0');
     
     begin
-        data_display <= data_read_r when ext = '1' else data_read_itnram;
-		  lb_n <= '0';
-		  ub_n <= '0';
-		  
-		  
-		  itnram: ram1 port map(addr, (not ext) and clk, data_write, not rw, data_read_itnram);
-		  
-		  mem_ext <= ext;
-        ctl: memorycontroller port map(clk, reset, addr, data_write, data_read_r, data_read, mem_ext, rw, ready, ad, dio, we_n, oe_n, ce_n);
-		  dp11:seg7 port map(addr(3 downto 0), segoutL(6 downto 0));
-		  dp12:seg7 port map(addr(7 downto 4), segoutL(13 downto 7));
-		  
-		  dp21:seg7 port map(data_write(3 downto 0), segoutM(6 downto 0));
-		  dp22:seg7 port map(data_write(7 downto 4), segoutM(13 downto 7));
-		  
-        dp13:seg7 port map(data_display(3 downto 0), segoutR(6 downto 0));
-        dp23:seg7 port map(data_display(7 downto 4), segoutR(13 downto 7));
-		  segoutR(27 downto 14) <= "10000001000000";
+        data_display <= data_read_r when ext = '1' else data_read_itnram; -- dio is useless
+        lb_n <= '0';
+        ub_n <= '0';
+        
+        itnram: ram1 port map(addr, (not ext) and clk, data_write, not rw, data_read_itnram);
+        
+        mem_ext <= ext;
+        ctl: memorycontroller port map(clk, reset_n, addr, data_write, data_read_r, data_read, mem_ext, rw, ready, ad, dio, we_n, oe_n, ce_n);
     end;
